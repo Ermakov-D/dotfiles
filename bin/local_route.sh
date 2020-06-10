9a6b4381304af4f1ae240398838c97655d85704f
Dmitriy Ermakov <ermakov@viam.ru>
2020-06-10 15:41:13 +0300

    2nd District

--- bin/local_route.sh
+++ bin/local_route.sh
@@ -3,6 +3,7 @@ sudo ip route add 192.168.0.0/22 via ${defGW}
 sudo ip route add 192.168.4.0/24 via ${defGW}
 sudo ip route add 192.168.30.0/24 via ${defGW}
 sudo ip route add 192.168.40.0/24 via ${defGW}
+sudo ip route add 10.254.2.0/30 via ${defGW}
 sudo ip route add 194.247.190.66 via ${defGW}
 sudo ip route add 194.85.88.0/24 via ${defGW}
 sudo ip route add 212.46.229.20/30 via ${defGW}
@@ -12,4 +13,4 @@ sudo ip route add 91.189.116.42 via ${defGW}
 sudo ip route add 91.189.116.40 via ${defGW}
 sudo ip route add 91.189.116.43 via ${defGW}
 sudo ip route add 91.189.116.41 via ${defGW}
-
+sudo ip route add 195.239.45.194 via ${defGW}
