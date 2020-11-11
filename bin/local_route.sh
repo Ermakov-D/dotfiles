414172501b2826740a63705ec5620a2dd10d340a
Dmitriy Ermakov <ermakov@viam.ru>
2020-11-11 16:32:04 +0300

    2-я территория

--- bin/local_route.sh
+++ bin/local_route.sh
@@ -1,6 +1,10 @@
 defGW=$(ip route list dev enp3s0 | awk ' /^default/ {print $3}' | tail -1)
 sudo ip route add 192.168.0.0/22 via ${defGW}
-sudo ip route add 192.168.4.0/24 via ${defGW}
+sudo ip route add 192.168.1.7/32 via 192.168.20.8 dev enp3s0
+sudo ip route add 192.168.3.36/32 via 192.168.20.8
+sudo ip route add 192.168.2.212/32 via 192.168.20.8
+sudo ip route add 192.168.2.228/32 via 192.168.20.8
+sudo ip route add 192.168.4.0/24 via 192.168.20.3
 sudo ip route add 192.168.30.0/24 via ${defGW}
 sudo ip route add 192.168.40.0/24 via ${defGW}
 sudo ip route add 10.254.2.0/30 via ${defGW}
