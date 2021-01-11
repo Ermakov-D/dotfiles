9bf68ffa65bfee9d6f7d43ccbbd6441d80b30934
Dmitriy Ermakov <ermakov@viam.ru>
2021-01-11 16:42:01 +0300

    2-я территория

--- bin/local_route.sh
+++ bin/local_route.sh
@@ -1,10 +1,10 @@
-defGW=$(ip route list dev enp3s0 | awk ' /^default/ {print $3}' | tail -1)
+defGW=$(ip route list dev eno1 | awk ' /^default/ {print $3}' | tail -1)
 sudo ip route add 192.168.0.0/22 via ${defGW}
 sudo ip route add 192.168.1.7/32 via 192.168.20.8 dev enp3s0
 sudo ip route add 192.168.3.36/32 via 192.168.20.8
 sudo ip route add 192.168.2.212/32 via 192.168.20.8
 sudo ip route add 192.168.2.228/32 via 192.168.20.8
-sudo ip route add 192.168.4.0/24 via 192.168.20.3
+sudo ip route add 192.168.4.0/24 via 192.168.20.8
 sudo ip route add 192.168.30.0/24 via ${defGW}
 sudo ip route add 192.168.40.0/24 via ${defGW}
 sudo ip route add 10.254.2.0/30 via ${defGW}
@@ -18,3 +18,4 @@ sudo ip route add 91.189.116.40 via ${defGW}
 sudo ip route add 91.189.116.43 via ${defGW}
 sudo ip route add 91.189.116.41 via ${defGW}
 sudo ip route add 195.239.45.194 via ${defGW}
+sudo ip route add 78.107.18.112/28 via ${defGW}
