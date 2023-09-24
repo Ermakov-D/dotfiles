18ce33a2d66c56013f560161a840705fd65c4c07
Dmitriy Ermakov <ermakov_d@mail.ru>
2023-09-24 11:52:45 +0300

    Изменение в i3lock

--- bin/screenlock.sh
+++ bin/screenlock.sh
@@ -35,33 +35,26 @@ else
     i3lock \
         --blur 5 \
         --bar-indicator \
-        --bar-position h \
+        --bar-pos h \
         --bar-direction 1 \
         --bar-max-height 50 \
         --bar-base-width 50 \
         --bar-color 000000cc \
-        --keyhlcolor 002F329F \
+        --keyhl-color 002F329F \
         --bar-periodic-step 50 \
         --bar-step 50 \
-        --bar-width 250 \
         --redraw-thread \
         \
         --clock \
         --force-clock \
-        #--timepos 5:h-80 \
-        #--timecolor 880088ff \
-        #--datepos tx:ty+15 \
-        #--datecolor 990099ff \
         --date-align 1 \
         --time-align 1 \
-        --ringvercolor 8800ff88 \
-        --ringwrongcolor ff008888 \
-        --statuspos 5:h-16 \
+        --ringver-color 8800ff88 \
+        --ringwrong-color ff008888 \
+        --status-pos 5:h-16 \
         --verif-align 1 \
         --wrong-align 1 \
-        --verifcolor ffffffff \
-        --wrongcolor ffffffff \
-        --modifpos -50:-50 \
+        --modif-pos -50:-50 \
         \
         --screen 1
 fi
