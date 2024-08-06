2bf31770a28e3cfbb4aeb0afb26cb8031df38371
Ermakov Dmitriy <ermakov.dmitriy@gmail.com>
2024-08-06 17:52:33 +0300

    Add info for byeDPI

---
+++ config/polybar/scripts/byeDPI.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+
+runningStatus() {
+    pid=$(pgrep ciadpi)
+
+ case "$connection" in
+    OK)
+	echo "1"
+	;;
+    *)
+	echo "2"
+	;;
+    esac
+}
+
+case "$1" in
+    --toggle)
+        if [ "$(connection_status)" = "1" ]; then
+            kill $(pgrep ciadpi)
+        else
+            ~/bin/ciadpi -p 3080 --disorder 1 --auto=torst --tlsrec 1+s
+    fi
+    ;;
+*)
+
+        ;;
+    *)
+	if [ "$(runningStatus)" = "1" ];then
+	    echo " ZeroTier"
+	else
+	    echo " ZeroTier"
+	fi
+esac
+
