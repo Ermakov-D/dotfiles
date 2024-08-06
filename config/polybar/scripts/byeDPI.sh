efa0427504eeb218775a7548636f0197f2223383
Depeche aka D.Ermakov <ermakov.dmitriy@gmail.com>
2024-08-06 21:26:10 +0300

    плагин для dpi

--- config/polybar/scripts/byeDPI.sh
+++ config/polybar/scripts/byeDPI.sh
@@ -2,34 +2,27 @@
 
 
 runningStatus() {
-    pid=$(pgrep ciadpi)
-
- case "$connection" in
-    OK)
-	echo "1"
-	;;
-    *)
-	echo "2"
-	;;
-    esac
+    if pgrep -x "ciadpi" >/dev/null
+    then
+        echo "1"
+    else
+        echo "2"
+    fi
 }
 
 case "$1" in
     --toggle)
-        if [ "$(connection_status)" = "1" ]; then
+        if [ $(runningStatus) = "1" ]; then
             kill $(pgrep ciadpi)
         else
-            ~/bin/ciadpi -p 3080 --disorder 1 --auto=torst --tlsrec 1+s
-    fi
-    ;;
-*)
-
+            ~/bin/ciadpi --ip 127.0.0.1 -p 3080 --disorder 1 --auto=torst --tlsrec 1+s &
+        fi
         ;;
     *)
-	if [ "$(runningStatus)" = "1" ];then
-	    echo " AntiDPI"
-	else
-	    echo " AntiDPI"
-	fi
+	    if [ $(runningStatus) = "1" ];then
+            echo " AntiDPI"
+	    else
+	        echo " AntiDPI"
+	    fi
 esac
 
