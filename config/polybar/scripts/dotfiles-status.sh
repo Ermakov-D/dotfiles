48021b83c953edc368fcaefb6c5916110035f623
Ermakov Dmitriy <ermakov.dmitriy@gmail.com>
2024-09-25 08:43:35 +0300

    Аплет для dotFile

--- config/polybar/scripts/dotfiles-status.sh
+++ config/polybar/scripts/dotfiles-status.sh
@@ -4,17 +4,20 @@ hostname=$(uname -n)
 
 case $hostname in
     Ser5)
-	dotfiles_dir="/home/depeche/.dotfiles/"
-	;;
+	    dotfiles_dir="/home/depeche/.dotfiles/"
+	    ;;
+    depeche-host)
+        dotfiles_dir="/home/depeche/.dotfile/"
+        ;;
     *)
 	dotfiles_dir="/home/depeche/.dotfile/"
 esac
-    
 
-if [ $(git -C ${dotfiles_dir} status --porcelain | wc -l) -eq "0" ]; then  
+
+if [ $(git -C ${dotfiles_dir} status --porcelain | wc -l) -eq "0" ]; then
   repo_status="local_ok"
-else		
-  repo_status="local_change"  
+else
+  repo_status="local_change"
 fi
 
 UPSTREAM=${1:-'@{u}'}
@@ -33,23 +36,23 @@ elif [ $REMOTE = $BASE ]; then
     echo -n "%{F#ff3205}: Push%{F-}"
     #echo -n " :Push"
 else
-    echo -n " :"    
+    echo -n " :"
 fi
 
 
-if [ $(git -C ${dotfiles_dir} status --porcelain | wc -l) -eq "0" ]; then  
+if [ $(git -C ${dotfiles_dir} status --porcelain | wc -l) -eq "0" ]; then
   echo
-else	
+else
 	change_file=$(git -C ${dotfiles_dir} status --porcelain | wc -l)
-	echo "  :${change_file}"	  
-    #echo "  :${change_file}"     
+	echo "  :${change_file}"
+    #echo "  :${change_file}"
 fi
 
 
 
 #case $repo_status in
 #
-#    "local_ok")	    
+#    "local_ok")
 #			echo
 #	    ;;
 #    "local_change")
@@ -57,6 +60,6 @@ fi
 #	    echo " : ${change_file}"
 #	    ;;
 #    *)
-#	    echo " o%{F#ef02db}: Push%{F-}"    
+#	    echo " o%{F#ef02db}: Push%{F-}"
 #	    ;;
 #esac
