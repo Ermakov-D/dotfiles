9f9e94d364bcd0bb44ed4293ff7ba2ed83ea8531
Ermakov Dmitriy <ermakov.dmitriy@gmail.com>
2024-09-25 10:29:16 +0300

    Аплет для dotFile

--- config/polybar/scripts/dotfiles-status.sh
+++ config/polybar/scripts/dotfiles-status.sh
@@ -21,20 +21,16 @@ else
 fi
 
 UPSTREAM=${1:-'@{u}'}
-LOCAL=$(cd ${dotfiles_dir}; git rev-parse @)
-REMOTE=$(cd ${dotfiles_dir}; git rev-parse "$UPSTREAM")
-BASE=$(cd ${dotfiles_dir}; git merge-base @ "$UPSTREAM")
+LOCAL=$(git -C ${dotfiles_dir} rev-parse @)
+REMOTE=$(git -C ${dotfiles_dir} rev-parse "$UPSTREAM")
+BASE=$(git -C ${dotfiles_dir} merge-base @ "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
     echo -n "%{F#6bff49}: Ok%{F-}"
-    #echo -n "%{F#6bff49}: Ok%{F-}"
-    #echo -n " :Ok"
 elif [ $LOCAL = $BASE ]; then
     echo -n "%{F#ff3205}: Pull%{F-}"
-    #echo -n " :Pull"
 elif [ $REMOTE = $BASE ]; then
     echo -n "%{F#ff3205}: Push%{F-}"
-    #echo -n " :Push"
 else
     echo -n " :"
 fi
@@ -45,21 +41,5 @@ if [ $(git -C ${dotfiles_dir} status --porcelain | wc -l) -eq "0" ]; then
 else
 	change_file=$(git -C ${dotfiles_dir} status --porcelain | wc -l)
 	echo "  :${change_file}"
-    #echo "  :${change_file}"
 fi
 
-
-
-#case $repo_status in
-#
-#    "local_ok")
-#			echo
-#	    ;;
-#    "local_change")
-#	    change_file=$(git status --porcelain | wc -l)
-#	    echo " : ${change_file}"
-#	    ;;
-#    *)
-#	    echo " o%{F#ef02db}: Push%{F-}"
-#	    ;;
-#esac
