50f46d0c94dd552211f5115ea9525860a07ff54b
Ermakov Dmitriy <ermakov.dmitriy@gmail.com>
2025-11-11 11:52:50 +0300

    xray info

--- config/polybar/scripts/xray-xhttp.sh
+++ config/polybar/scripts/xray-xhttp.sh
@@ -13,14 +13,14 @@ case "$1" in
         if [ $(runningStatus) = "xray" ] ; then
             kill $(pgrep xray)
         else
-            /usr/bin/xray -c /home/depeche/Nextcloud/VPNKeys/xray-xhttp.json
+            /usr/bin/xray -c /home/depeche/Nextcloud/VPNKeys/xray-xhttp.json >/dev/null 2>&1 &
         fi
         ;;
     *)
         case "$(runningStatus)" in
             xray)
-                proxyIP=$(curl -x socks5h://127.0.0.1:2082 -s ifconfig.io/ip)
-                echo " [xray: $proxyIP]"
+                proxyIP=$(timeout 5 curl -x socks5h://127.0.0.1:2082 -s ifconfig.io/ip)
+                echo " [s2082: $proxyIP]"
                 ;;
             *)
                 echo " xray"
